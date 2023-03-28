fb803a9af5844004cb2a255cad08d2185430837a
deborawein <debiwein@gmail.com>
2023-03-28 21:37:36 +1100

    second commit

---
+++ screens/SignUp.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export function SignUpScreen ( props ) {
+    return (
+      <View>
+        <Text>Sign Up Screen</Text>
+      </View>
+    );
+  }
