7a61df639fc625d89b80385bedccf0e4ee348be2
deborawein <debiwein@gmail.com>
2023-03-28 22:13:16 +1100

    Sign Up Screen

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,9 +1,51 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { TextInput } from "react-native";
 
 export function SignUpScreen ( props ) {
     return (
-      <View>
-        <Text>Sign Up Screen</Text>
+      <View style={styles.page}>
+        <Text style={styles.title}>Sign Up Screen</Text>
+        <View style={styles.inputGroup}>
+            <Text>Email address</Text>
+            <TextInput style={styles.input} />
+        </View>
+        <View style={styles.inputGroup}>
+            <Text>Password</Text>
+            <TextInput style={styles.input} />
+        </View>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+
+        </TouchableOpacity>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+
+    page: {
+      marginHorizontal: 60,  
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    input: {
+        backgroundColor: '#ffffff',
+        padding: 5,
+
+    },
+    inputGroup: {
+        marginVertical: 5,
+    },
+    button: {
+        backgroundColor: '#000000',
+        padding: 10,
+        marginVertical: 10;
+    },
+    buttonText: {
+        color: '#ffffff',
+        textAlign: 'center',
+    }
+  })
